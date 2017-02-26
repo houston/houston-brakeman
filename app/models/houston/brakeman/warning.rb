bb09dc4c329293ed76b57c843a0f1465fbb970c5
Bob Lail <bob.lailfamily@gmail.com>
2017-02-26 15:39:09 -0600

    [skip] Added validations for warnings (1m)

--- app/models/houston/brakeman/warning.rb
+++ app/models/houston/brakeman/warning.rb
@@ -5,12 +5,22 @@ module Houston
 
       belongs_to :project
 
+      validates :project_id,
+                :warning_type,
+                :warning_code,
+                :fingerprint,
+                :message,
+                :file,
+                :url,
+                :confidence,
+                presence: true
+
       def link=(value)
         self.url = value
       end
 
       def self.find_or_create_for(warning)
-        create_with(warning).find_or_create_by(fingerprint: warning["fingerprint"])
+        create_with(warning).find_or_create_by!(fingerprint: warning["fingerprint"])
       rescue ActiveRecord::RecordNotUnique
         find_by(fingerprint: warning["fingerprint"])
       end
